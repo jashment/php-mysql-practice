f19c04b60617ddcc2b8db98603f7553cb45cf5b5
Jordan <jordanashment@gmail.com>
2019-05-22 15:40:41 +0000

    Very simple PHP

--- hello-world.php
+++ hello-world.php
@@ -1,12 +1,43 @@
 <html>
     <body>
         <?php
-        // A simple web site in Cloud9 that runs through Apache
-        // Press the 'Run' button on the top to start the web server,
-        // then click the URL that is emitted to the Output tab of the console
-        
-            echo 'Hello World!';
+            $name = "Jordan Ashment";
+            
+            $poem = "Roses are red, Violets are blue";
+            $poem .= "</br>This poem sucks.";
+            
+            $family = array("Mark", "Joan", "Rick", "Brett", "Kerry", "Kevin");
+            $vehicles[0] = "car";
+            $vehicles[1] = "airplane";
+            $vehicles[2] = "X-wing";
+            $vehicles[3] = "Tardis";
+            
 
+            print($family[0] . " just purchased a new " . $vehicles[2]);
+            print("<br/>");
+            echo 'Hello World!';
+            echo("<br/>");
+            // printf("<br/> I am %d years old", 28);
+            // printf("<br/> My name is $name")
+            
+
+            echo $poem;
+            echo("<br/>");
+            
+            for($i = 0; $i < count($family); $i++) {
+                print($family[$i]);
+                print("<br/>");
+            }
+            
+            // Declare array
+            $gpas = (
+                    "Smith" => 3.25,
+                    "Johnson" => 3.1,
+                    "Simpson" => 1.6,
+                    "Musk" => 4.0
+                )
+            
+            
         ?>
         <form>
                 <input type="button" value="Click me!" onclick="window.location.href='https://www.google.com'" />
