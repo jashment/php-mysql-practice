76e4c77b8ceeddc6a7a40089769377d77df59907
Jordan <jordanashment@gmail.com>
2019-05-22 14:43:30 +0000

    Initial Commit

---
+++ hello-world.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <?php
+        // A simple web site in Cloud9 that runs through Apache
+        // Press the 'Run' button on the top to start the web server,
+        // then click the URL that is emitted to the Output tab of the console
+        
+            echo 'Hello World!';
+
+        ?>
+        <form>
+                <input type="button" value="Click me!" onclick="window.location.href='https://www.google.com'" />
+            </form>
+    </body>
+</html>
