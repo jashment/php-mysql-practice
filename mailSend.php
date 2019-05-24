65d88a47002b2ff6f2d48a16fc2a9a19260f0295
Jordan <jordanashment@gmail.com>
2019-05-24 15:31:42 +0000

    Sending emails in PHP

--- mailSend.php
+++ mailSend.php
@@ -7,18 +7,31 @@
         <?php
 
             // Basic text email
+            // $to = "jashment22@gmail.com";
+            // $subject = "Feedback";
+            // $message = "This is a test email using PHP.";
+            // $message .= "It's just testing email sending capabilities.";
+            // $from = "jashment22@gmail.com";
+            
+            // $headers = "From: " . $from;
+            // mail($to, $subject, $message, $headers);
+            // echo("Mail has been sent.");
+            
+            //HTML emails
             $to = "jashment22@gmail.com";
-            $subject = "Feedback";
-            $message = "This is a test email using PHP.";
-            $message .= "It's just testing email sending capabilities.";
             $from = "jashment22@gmail.com";
             
-            $headers = "From: " . $from;
+            $headers = "From: " . $from . "\r\n";
+            $headers = "Reply-To " . $from . "\r\n";
+            $headers = "MIME-Version : 1.0\r\n";
+            $headers = "Content-Type: text/html; charset=ISO-8859-1\r\n";
+            
+            $message = "<html><body>";
+            $message = "<h1> Information From Myself</h1>";
+            $message = "<p>It's time to act!</p>";
+            
             mail($to, $subject, $message, $headers);
             echo("Mail has been sent.");
-            
-            
-            
         ?>
     </body>
 </html>
